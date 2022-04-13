62cf5bfa206aaf06f6fe387a24a10fd96d1f666c
LOapp-KOH <kkiho19@gmail.com>
2022-04-13 13:08:55 +0000

    d

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 var num=prompt("숫자 입력하세요");
 var i=1;
-
+document.write("dheh");
 while(i<num)
 {
     document.write(i+", ")
     i*=3;
 }
 res=num/3;
-document.write("<p id="emp">"+"num+"까지 3의 배수의 개수 : "+res+"</p>);
+document.write("<p id="oi1">"+"num"+"까지 3의 배수의 개수 : "+res+"</p>");
